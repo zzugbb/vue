0134d00c73246345eefbf0ab32ca23699674e67f
zzugbb <1019178691@qq.com>
2019-04-18 14:04:36 +0800

    菜单

--- my-manage-system/src/components/menu.vue
+++ my-manage-system/src/components/menu.vue
@@ -1,20 +1,35 @@
 <template>
   <el-menu
-    default-active="2"
+    default-active="1"
     class="el-menu-vertical-demo"
-    @open="handleOpen"
-    @close="handleClose">
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    :collapse="menuCollapse"
+    style="height:100vh;background-color:#545c64"
+    >
+    <el-menu-item style="height:120px;">
+      <template>
+        <div class="menu-user">
+          <div class="logo-div">
+            <img src="../assets/logo.png" class="logo"/>
+            <span>后台系统</span>
+          </div>
+          <img src="../assets/avator.jpg" class="avator"/>
+        </div>  
+      </template>
+    </el-menu-item>
     <el-menu-item index="1">
       <i class="el-icon-location"></i>
-      <span>导航一</span>
+      <span>账单列表</span>
     </el-menu-item>
     <el-menu-item index="2">
       <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
+      <span slot="title">账单新增</span>
     </el-menu-item>
     <el-menu-item index="4">
       <i class="el-icon-setting"></i>
-      <span slot="title">导航三</span>
+      <span slot="title">我的</span>
     </el-menu-item>
   </el-menu>
 </template>
@@ -22,13 +37,33 @@
 <script>
 export default {
   name: "Menu",
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+  props: ['menuCollapse'],
+  data() {
+    return {
     }
   }
 }
 </script>
+
+<style lang="less">
+
+.menu-user {
+  text-align: center;
+  color: #fff;
+  font-size: 25px;
+
+  .logo {
+    width: 30px;
+    height: 30px;
+    padding-right: 15px;
+    vertical-align: middle;
+  };
+
+  .avator {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
+}
+</style>
+
--- my-manage-system/src/views/Home.vue
+++ my-manage-system/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div> 
     <el-row>
-      <el-col :span="4">
-        <Menu/>
+      <el-col :span="isCollapse ? 1 : 4">
+        <Menu :menuCollapse="isCollapse"/>
       </el-col>
-      <el-col :span="20">
-        欢迎来到 xx 系统
+      <el-col :span="isCollapse ? 23: 20">
+        <div class="header">
+          <img src="../assets/menu-open.png" @click="menuCollapse()" v-show="!isCollapse">
+          <img src="../assets/menu-close.png" @click="menuCollapse()" v-show="isCollapse">
+          <el-button class="login-out">退出</el-button>
+        </div>
       </el-col> 
     </el-row>
   </div>
@@ -13,13 +17,47 @@
 </template>
 
 <script>
-import Menu from "@/components/menu"
+import Menu from "@/components/Menu"
 
 export default {
   name: 'Home',
   components: {
     Menu
+  },
+  data() {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    menuCollapse() {
+      this.isCollapse = ! this.isCollapse
+    }
   }
 }
 </script>
+
+<style lang="less">
+.header {
+  height: 50px;
+  line-height: 50px;
+  background-color: #fff;
+  box-shadow: 0px 0px 5px #888888;
+  img {
+    width: 25px;
+    height: 25px;
+    margin-top: 12px;
+    margin-left: 15px;
+    cursor: pointer;
+  }
+  .login-out {
+    float: right;
+    height: 30px;
+    line-height: 0;
+    margin-right: 30px;
+    margin-top: 10px;
+  }
+}
+</style>
+
 
